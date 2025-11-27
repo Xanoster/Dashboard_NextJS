b0047f7fca4531905d217fc4c904211e74a6e804
Xanoster <techypankaj@gmail.com>
2025-11-27 14:29:09 +0100

    Add search functionality to customers page

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,16 +1,27 @@
 import { lusitana } from '@/app/ui/fonts';
 import CustomersTable from '@/app/ui/customers/table';
+import Search from '@/app/ui/search';
 import { Suspense } from 'react';
 import { fetchFilteredCustomers } from '@/app/lib/data';
 
-export default async function Page() {
-  const customers = await fetchFilteredCustomers('');
+export default async function Page(props: {
+  searchParams?: Promise<{
+    query?: string;
+  }>;
+}) {
+  const searchParams = await props.searchParams;
+  const query = searchParams?.query || '';
+  
+  const customers = await fetchFilteredCustomers(query);
 
   return (
     <div className="w-full">
-      <h1 className={`${lusitana.className} mb-8 text-xl md:text-2xl`}>
-        Customers
-      </h1>
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Customers</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search customers..." />
+      </div>
       <Suspense fallback={<div>Loading...</div>}>
         <CustomersTable customers={customers} />
       </Suspense>
